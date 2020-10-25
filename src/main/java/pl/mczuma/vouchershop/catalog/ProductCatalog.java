e40faf2092d0b5625b15091638bb39cd62ecdad5
m3uma <60619567+m3uma@users.noreply.github.com>
2020-10-25 18:26:29 +0100

    add spec to product catalog

--- src/main/java/pl/mczuma/vouchershop/catalog/ProductCatalog.java
+++ src/main/java/pl/mczuma/vouchershop/catalog/ProductCatalog.java
@@ -1,17 +1,48 @@
 package pl.mczuma.vouchershop.catalog;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ProductCatalog {
+    private HashMap<String, Product> products;
+
+    public ProductCatalog() {
+        this.products = new HashMap<String, Product>();
+    }
+
     public String registerProduct() {
-        return UUID.randomUUID().toString();
+        Product newProduct = new Product(UUID.randomUUID());
+        products.put(newProduct.getId(), newProduct);
+        return newProduct.getId();
     }
 
     public boolean isExists(String productId) {
-        return true;
+        return products.containsKey(productId);
     }
 
     public Product load(String productId) {
-        return null;
+        return products.get(productId);
+    }
+
+    public void updateDetails(String productId, String productDesc, String productPicture) {
+        Product loaded = products.get(productId);
+        loaded.setDescription(productDesc);
+        loaded.setPicture(productPicture);
+    }
+
+    public void applyPrice(String productId, BigDecimal price) {
+        Product loaded = products.get(productId);
+        loaded.setPrice(price);
+    }
+
+    public List<Product> allPublished() {
+        return products.values()
+                .stream()
+                .filter(p -> p.getDescription() != null)
+                .filter(p -> p.getPicture() != null)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/pl/mczuma/vouchershop/catalog/ProductCatalogTest.java
+++ src/test/java/pl/mczuma/vouchershop/catalog/ProductCatalogTest.java
@@ -3,16 +3,21 @@ package pl.mczuma.vouchershop.catalog;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class ProductCatalogTest {
     public static final String NOT_EXISTS_ID = "notExistsId";
+    public static final String PRODUCT_DESC = "My nice product";
+    public static final String PRODUCT_PICTURE = "http://nice_picture";
 
     @Test
     public void itAllowsToRegisterNewProduct(){
-        //Arrange
+        //Arrange //Given
         ProductCatalog catalog = thereIsProductCatalog();
-        //Act
+        //Act //When //Act
         String productId = catalog.registerProduct();
-        //Assert
+        //Assert //Then //Expected
         Assert.assertTrue(catalog.isExists(productId));
         Assert.assertFalse(catalog.isExists(NOT_EXISTS_ID));
     }
@@ -30,16 +35,44 @@ public class ProductCatalogTest {
     }
 
     @Test
-    public void itAllowsFillADtais(){
+    public void itAllowsFillADetais(){
         //Arrange
         ProductCatalog catalog = thereIsProductCatalog();
         //Act
         String productId = catalog.registerProduct();
-        catalog.updateDetails(productID, "My nice product", "http://nice_picture");
+        catalog.updateDetails(productId, PRODUCT_DESC, PRODUCT_PICTURE);
         Product loaded = catalog.load(productId);
         //Assert
-        Assert.assertEquals(loaded.getPicture(), "http://nice_picture");
-        Assert.assertEquals(loaded.getDescription(), "http://nice_picture");
+        Assert.assertEquals(loaded.getPicture(), PRODUCT_PICTURE);
+        Assert.assertEquals(loaded.getDescription(), PRODUCT_DESC);
+    }
+
+    @Test
+    public void itAllowsApplyPrice(){
+        //Arrange
+        ProductCatalog catalog = thereIsProductCatalog();
+        //Act
+        String productId = catalog.registerProduct();
+        catalog.applyPrice(productId, BigDecimal.TEN);
+        Product loaded = catalog.load(productId);
+        //Assert
+        Assert.assertEquals(loaded.getPrice(), BigDecimal.TEN);
+    }
+
+    @Test
+    public void itAllowsToListAllProducts(){
+        //Arrange
+        ProductCatalog catalog = thereIsProductCatalog();
+        //Act
+        String productId = catalog.registerProduct();
+        catalog.updateDetails(productId, PRODUCT_DESC, PRODUCT_PICTURE);
+        catalog.applyPrice(productId, BigDecimal.TEN);
+
+        //Assert
+        String draftProductId = catalog.registerProduct();
+        List<Product> all = catalog.allPublished();
+
+        Assert.assertEquals(1, all.size());
     }
 
     private static ProductCatalog thereIsProductCatalog() {
